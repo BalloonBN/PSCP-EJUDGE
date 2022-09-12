584a2989ab7822a2ed0aa3934489025d927be86b
baloonbnkt1 <98270474+baloonbnkt1@users.noreply.github.com>
2022-09-12 17:29:37 +0700

    First Commit

---
+++ 85.Run Length Decoding.py
@@ -0,0 +1,13 @@
+"""Ejudge"""
+def main():
+    """Run Length Decoding"""
+    coding = input()
+    decode = ""
+    for i in coding:
+        if i.isdigit():
+            decode += i
+        else:
+            print(i * int(decode), end="")
+            decode = ""
+main()
+
